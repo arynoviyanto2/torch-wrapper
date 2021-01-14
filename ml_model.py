f80362dda5d6ea5fb23f8244f0a4b28dd97eaf25
arynoviyanto <arynoviyanto@gmail.com>
2021-01-15 07:29:30 +1300

    adjust optimiser parameter

--- ml_model.py
+++ ml_model.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import numpy as np
 import torch.optim as optim
 
@@ -12,9 +13,12 @@ class MlModel:
         return self.model
         
     def loss_func(self, batch_outputs, batch_labels):
-        batch_size = batch_outputs.size()[0]
-        return -torch.sum(batch_outputs[range(batch_size), batch_labels]) / batch_size
+        loss = nn.CrossEntropyLoss()
+        return loss(batch_outputs, batch_labels)
+        # batch_size = batch_outputs.size()[0]
+        # return -torch.sum(batch_outputs[range(batch_size), batch_labels]) / batch_size
 
     def optimizer(self, params=None):
-        return optim.Adam(self.model.parameters(), lr=0.1)
+        # return optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9) 
+        return optim.Adam(self.model.parameters(), lr=0.0001)
         
